0ed19a70a0c25a70882a7c93aef516ebff19b342
Matthew Shepherd <matthewshepherd@matthews-air.dhcp.wustl.edu>
2017-01-28 15:05:47 -0600

    recent pull

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/michaelgreer/Desktop/WURacing/Dashboard/venv"
+set -gx VIRTUAL_ENV "/Users/matthewshepherd/Documents/Schoolwork/2 - Sophomore Year/Organizations/WU Racing/Dashboard/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
