4e1c624d099f7acccea5d70f35a83394efa6de65
rh116 <renhao.liu@wustl.edu>
2017-11-11 19:25:35 -0600

    global_val added

---
+++ global_vars.py
@@ -0,0 +1,23 @@
+import os
+from datetime import datetime
+
+def init():
+	global prefixes
+	prefixes = ["RPMs","Load","Throttle","Coolant","O2","Speed","Gear","Volts","RRPot","RLPot"]
+
+	global primaries
+	primaries = ["RPMs","Load","Throttle","RRPot","RLPot"]
+
+	global secondaries
+	secondaries = ["Coolant","O2","Speed","Gear","Volts"]
+
+	global data
+	data = {prefix: 0 for prefix in prefixes}
+
+	sep = "\\" if os.name == "nt" else "/"
+	directory = os.getcwd()+sep+"logs"+sep+"Engine Data "+datetime.today().strftime('%m-%d-%Y')+" "+datetime.now().strftime('%H:%M:%S')+sep
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+
+	global filenames
+	filenames = {prefix: directory+prefix+".csv" for prefix in prefixes}
